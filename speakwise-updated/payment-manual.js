6f16145002a8f138c7dc4728a1b2cd0cf9a60246
Rahul Dobriyal <rahuldobriyal@outlook.com>
2026-06-23 10:51:52 +0530

    Update payment-manual.js

--- speakwise-updated/payment-manual.js
+++ speakwise-updated/payment-manual.js
@@ -66,7 +66,7 @@ upiOptions.innerHTML = PAYMENT_CONFIG.upiOptions.map((option) => `
 
 const upiUrl =
   `upi://pay?pa=${primaryUpi.id}` +
-  `&pn=NIRMAL SHARMA` +
+  `&pn=NIRMALSHARMA` +
   `&am=${selectedPlan.amount}` +
   `&cu=INR` +
   `&tn=${selectedPlan.name}`;
